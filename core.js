1211d5ce6e6c56ef67dcc7e4949c1248617f7764
William Zhao <wzhao2017@gmail.com>
2017-10-15 01:23:12 -0400

    Final Commit Because of Caps
    Need to change repository

--- core.js
+++ core.js
@@ -10,7 +10,31 @@ var $ = require('jQuery');
 
 
 var request = require('superagent');
-request.get('http://api.reimaginebanking.com/merchants?key=bfb89f2b1bfd56ed997cf7e93471eaed').end(function(err, res){
-    console.log(res.body.data[0]._id);
+request.get('http://api.reimaginebanking.com/accounts?key=bfb89f2b1bfd56ed997cf7e93471eaed').end(function(err, res){
+    console.log(res.body[0]._id);
 });
+
+var customer_id;
+var name;
+
+function pickDortha(){
+    window.location.href = "customer.html";
+    customer_id = "59e18455a73e4942cdafe19a";
+    name = "Dortha Mertz";
+    customer.html.getElementById('person').innerHTML = name;
+}
+
+function pickRandal(){
+    window.location.href = "customer.html";
+    customer_id = "59e18456a73e4942cdafe19b";
+    name = "Randal Schlutz";
+}
+
+function pickDarrel(){
+    window.location.href = "customer.html";
+    customer_id = "59e18455a73e4942cdafe19c";
+    name = "Darrel Powlowski";
+}
+
+
 
